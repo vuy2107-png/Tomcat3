44b7102a5a19df1051d68a680b617b689ed31f8f
Vũ Ngọc Duy <vuy2107@gmail.com>
2025-12-04 15:56:39 +0700

    uppp

---
+++ jwbd-servlet-calculator/src/main/java/servlet/CalculatorServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import model.Calculator;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CalculatorServlet", urlPatterns = "/calculate")
+public class CalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float firstOperand = Float.parseFloat(request.getParameter("first-operand"));
+        float secondOperand = Float.parseFloat(request.getParameter("second-operand"));
+        char operator = request.getParameter("operator").charAt(0);
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Result:</h1>");
+
+        try {
+            float result = Calculator.calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        } catch (Exception ex) {
+            writer.println("Error: " + ex.getMessage());
+        }
+
+        writer.println("</html>");
+    }
+}
